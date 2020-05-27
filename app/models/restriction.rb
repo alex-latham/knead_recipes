19b8f1291897a164023f9c4b0c00a10fc979400b
Alex Latham <alexlthm@gmail.com>
2020-05-27 17:45:33 -0600

    Configure rubocop, fix citations

--- app/models/restriction.rb
+++ app/models/restriction.rb
@@ -1,5 +1,5 @@
 class Restriction < ApplicationRecord
-  has_many :user_restrictions
+  has_many :user_restrictions, dependent: :destroy
   has_many :users, through: :user_restrictions
 
   validates :type, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :favorites
-  has_many :user_restrictions
+  has_many :favorites, dependent: :destroy
+  has_many :user_restrictions, dependent: :destroy
   has_many :restrictions, through: :user_restrictions
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
