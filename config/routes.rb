ca93bc07800aaeba71725a32824ecebe14753d7b
Fred Rondina <fredrondina96@gmail.com>
2020-05-28 17:24:03 -0600

    added routes for user profile, viewing friends, and adding friends

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
-
+  get '/profile', to: 'users#show'
+  get '/profile/friends', to: 'friends#index'
+  post '/profile/friends', to: 'friends#create'
   resources :welcome, only: [:index, :show]
 end
