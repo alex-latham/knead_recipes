a6c7681a52665c9335343bca1d0f0f1907ed1a72
David Tran <davidttemail@gmail.com>
2020-05-28 21:24:12 -0600

    Add MVC for recipes index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
+  resources :search, only: [:index, :create]
+
+  resources :recipes, only: [:index]
+
   resources :welcome, only: [:index, :show]
 end
