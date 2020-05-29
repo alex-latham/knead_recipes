2183e8adca0dec81c82223ce8df1a139630640d3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-28 18:34:17 -0600

    Remove validation for bio

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:bio) }
   end
 
   describe 'relationships' do
