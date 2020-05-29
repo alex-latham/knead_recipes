1fac63084700f946e32d8789b38b7e166af4f06c
Fred Rondina <fredrondina96@gmail.com>
2020-05-29 16:42:45 -0600

    added sad path test for trying to friend an already existing friend

--- spec/features/friends/user_can_add_friends_spec.rb
+++ spec/features/friends/user_can_add_friends_spec.rb
@@ -29,6 +29,24 @@ describe "As a user" do
     fill_in "Email Address", with: "asfasdf"
     click_button "Add Friend"
     expect(page).to_not have_content("Friend Added Successfully")
-    expect(page).to_not have_content("Invalid Email Entered, Try Again")
+    expect(page).to have_content("Invalid Email Entered, Try Again")
+  end
+
+  it "I cannot add a friend that is already on my friends list" do
+    user_1 = User.create!(name: "F", email: "F@example.com", bio: "Fun Guy")
+    user_2 = User.create!(name: "G", email: "G@example.com", bio: "Also a Fun Guy")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit "/profile/friends"
+
+    fill_in "Email Address", with: user_2.email
+    click_button "Add Friend"
+    expect(current_path).to eq("/profile/friends")
+    expect(page).to have_content("Friend Added Successfully")
+
+    fill_in "Email Address", with: user_2.email
+    click_button "Add Friend"
+    expect(page).to have_content("Invalid Email Entered, Try Again")
   end
 end
