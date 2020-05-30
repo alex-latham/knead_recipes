34a314ece022094156059eddc3fdc04d76e4bd51
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-29 19:40:57 -0600

    Update test with basic functionality

--- spec/features/users/user_profile_page_spec.rb
+++ spec/features/users/user_profile_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'as a registered user I have a login page' do
   it 'is not visible if I am not logged in' do
-    visit profile_path
+    visit '/profile'
 
     expect(page).to_not have_css("user-name")
     expect(page).to_not have_css("user-bio")
@@ -13,12 +13,23 @@ describe 'as a registered user I have a login page' do
     expect(page).to have_content("404")
   end
 
-  it 'can be seen when I log in via oauth' do
-
-  end
+  # it 'can be seen when I log in via oauth' do
+  #
+  # end
 
   it 'has my information and links to edit info, my favroties, and my friends' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/profile'
 
+    expect(page).to have_content("User Profile")
+
+    # expect(page).to_not have_css("user-name")
+    # expect(page).to_not have_css("user-bio")
+    # expect(page).to_not have_css("user-email")
+    # expect(page).to_not have_link("Edit Profile")
+    # expect(page).to_not have_link("Favorites")
+    # expect(page).to_not have_link("Freinds")
   end
 end
 
