b9c4e2311ea5b6247d4a2bb25446420bf2fa1896
David Tran <davidttemail@gmail.com>
2020-05-29 20:23:31 -0600

    Recreate cassettes, sad paths

--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -5,14 +5,12 @@ describe "searching recipes" do
     VCR.use_cassette('apples_cinnamon_vegetarian_time_15_dessert_3_results') do
       visit "/search"
       fill_in :ingredients, with: "apples, cinnamon"
-      check(:vegetarian)
-
+      find(:xpath, "//input[@value='vegetarian']").click
       find(:xpath, "//input[@value='15']").click
       find(:xpath, "//input[@value='dessert']").click
 
-
       click_on "Search Recipes"
-      expect(page).to have_current_path("/recipes?ingredients=apples%2C+cinnamon&time=15&type=dessert&vegetarian=true")
+      expect(page).to have_current_path("/recipes?diet%5B%5D=vegetarian&ingredients=apples%2C+cinnamon&time=15&type=dessert")
       within(".recipes") do
         expect(page).to have_css('.recipe-image', count: 3)
       end
@@ -26,6 +24,19 @@ describe "searching recipes" do
       within(".recipes") do
         expect(page).to have_css('.recipe-image', count: 10)
       end
+    end
+  end
+
+  it "doesn't return recipes with contradicting parameters" do
+    VCR.use_cassette("pork_vegetarian") do
+      visit "/search"
+      fill_in :ingredients, with: "pork"
+      find(:xpath, "//input[@value='vegetarian']").click
+      click_on "Search Recipes"
+
+      expect(page).to have_content("Sorry, we couldn't find any matching your specification. Here's some random recipes you might like.")
+      expect(page).to have_css('.recipe-image', count: 10)
+      expect(page).to have_css('.recipe-title', count: 10)
     end
   end
 end
