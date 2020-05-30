7f6a5a3573c2af99dbb217f04a45bdf70bd156be
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-29 22:42:42 -0600

    Add links to pages on homepage

--- app/controllers/profile/users_controller.rb
+++ app/controllers/profile/users_controller.rb
@@ -9,9 +9,18 @@ class Profile::UsersController < ApplicationController
     @user = current_user
   end
 
+  def update
+    require "pry"; binding.pry
+    redirect_to profile_path
+  end
+
   private
 
   def require_user
     render file: "/public/404" unless current_user
+  end
+
+  def update_params
+    params.permit(:vegetarian, :gluten_free, :vegan, :diary_free, :keto)
   end
 end
