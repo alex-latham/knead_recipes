db955a46363eba31eb5c5349edfe7a0f98563c57
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-29 22:42:55 -0600

    Create test to edit user profiles

---
+++ spec/features/users/user_edit_profile_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'as a user when I visit my profile' do
+  it 'there is a link to edit my profile' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit profile_path
+
+    click_link "Edit Profile"
+      expect(current_path).to eq(profile_edit_path)
+
+    visit profile_path
+  end
+
+  it 'I can see my current dietary preferences' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit profile_edit_path
+
+    expect(page).to have_content user.name
+
+    expect(page).to have_content user.email
+
+    check 'vegetarian'
+    # check 'gluten_free'
+
+    click_button "Update Profile"
+
+    expect(current_path).to eq profile_path
+    expect(page).to have_content("vegetarian")
+  end
+end
