11656ca2dd5225858d841c1cd954207d0876c9e1
Fred Rondina <fredrondina96@gmail.com>
2020-05-30 12:27:24 -0600

    master successfully merged in, 100% coverage, 2 rubocop violations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
-  get '/profile', to: 'users#show'
   get '/profile/friends', to: 'friends#index'
   post '/profile/friends', to: 'friends#create'
   resources :welcome, only: [:index, :show]
@@ -21,6 +20,8 @@ Rails.application.routes.draw do
     get '/', to: 'users#show'
     get '/edit', to: 'users#edit'
     patch '/edit/:id', to: 'users#update'
+    # get '/friends', to: 'friends#index'
+    # post '/friends', to: 'friends#create'
   end
 
   get 'auth/google_oauth2', as: 'google_login'
