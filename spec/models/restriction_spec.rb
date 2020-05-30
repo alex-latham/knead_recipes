3e57086c2f0cc11b72ff5ce27713b9341ed187e1
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-29 23:10:23 -0600

    Rename type to name on restriction model

--- spec/models/restriction_spec.rb
+++ spec/models/restriction_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Restriction, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:name) }
   end
 
   describe 'relationships' do
