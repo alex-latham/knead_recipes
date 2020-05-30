6015d880cd4f6b4bb86306b1e2c3eb5078a49e28
David Tran <davidttemail@gmail.com>
2020-05-30 01:26:37 -0600

    Refactor MVC

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,12 @@
+require './app/models/poros/recipe'
+
 class RecipesController < ApplicationController
   def index
-    connection = Faraday.new(url: 'https://api.spoonacular.com/',
-                             params: request_params)
-    request = connection.get('/recipes/complexSearch')
-    @recipes = JSON.parse(request.body, symbolize_names: true)
-    if @recipes[:totalResults] == 0
+    recipes = SpoonacularService.new(search_params).get_recipes
+    @recipes = recipes[:results].map do |recipe_json|
+      Recipe.new(recipe_json)
+    end
+    if @recipes.size == 0
       flash[:error] = "Sorry, we couldn't find any recipes matching your
         specification. Here's some random recipes you might like."
       redirect_to "/recipes"
@@ -13,32 +15,10 @@ class RecipesController < ApplicationController
 
   private
 
-  def request_params
-    { apiKey: ENV['SPOONACULAR_KEY'],
-      addRecipeInformation: true,
-      includeIngredients: format_params['ingredients'],
-      fillIngredients: true, ignorePantry: true,
-      maxReadyTime: format_params['time'], diet: format_params['diet'],
-      type: format_params['type'],
-      minCalories: 0, minCholesterol: 0,
-      minFat: 0, minProtein: 0, minCarbs: 0, minSugar: 0, minSodium: 0,
-      number: 10, sort: 'random' }.compact
-  end
-
   def search_params
     params.permit(:ingredients,
                   :time,
                   :type,
                   :diet => [])
-  end
-
-  def format_params
-    search_params.to_h.each_with_object({}) do |(key, value), acc|
-      if key == 'diet'
-        acc[key] = value.join(',')
-      else
-        key == 'time' ? acc[key] = value.to_i : acc[key] = value
-      end
-    end
   end
 end
