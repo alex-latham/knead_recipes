3342f57c07443828eb5f230ba4dabdd5d68b087f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-30 20:18:18 -0600

    Get beginnings of a working test

---
+++ spec/features/search/diet_preferences_selected_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'user preferences in search' do
+  it 'will automatically select my dietary preferences' do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+
+    veg = find(:xpath, "//input[@value='vegetarian']")
+    vegan = find(:xpath, "//input[@value='vegan']")
+    dairy = find(:xpath, "//input[@value='dairy free']")
+    
+    expect(veg).to_not be_checked
+    expect(vegan).to_not be_checked
+    expect(dairy).to_not be_checked
+
+    veg = Restriction.create(name: 'vegetarian')
+    gf = Restriction.create(name: 'gluten_free')
+    user.restrictions << veg
+    user.restrictions << gf
+
+    visit root_path
+    expect(veg).to_not be_checked
+    expect(vegan).to_not be_checked
+    expect(dairy).to be_checked
+  end
+end
