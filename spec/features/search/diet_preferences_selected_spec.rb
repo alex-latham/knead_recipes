9d0a284075fce2a83d152e3b0a9e0263ef7cfebf
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-30 20:27:53 -0600

    Set up test for eventual success

--- spec/features/search/diet_preferences_selected_spec.rb
+++ spec/features/search/diet_preferences_selected_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe 'user preferences in search' do
   it 'will automatically select my dietary preferences' do
     user = create(:user)
+    # veggie = Restriction.create(name: 'vegetarian')
+    dairy_free = Restriction.create(name: 'dairy_free')
+    user.restrictions << dairy_free
+    # user.restrictions << veggie
+    # user.restrictions << gf
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -12,16 +17,12 @@ describe 'user preferences in search' do
     vegan = find(:xpath, "//input[@value='vegan']")
     dairy = find(:xpath, "//input[@value='dairy free']")
 
-    expect(veg).to_not be_checked
-    expect(vegan).to_not be_checked
-    expect(dairy).to_not be_checked
+    # expect(veg).to_not be_checked
+    # expect(vegan).to_not be_checked
+    # expect(dairy).to_not be_checked
 
-    veggie = Restriction.create(name: 'vegetarian')
-    gf = Restriction.create(name: 'gluten_free')
-    user.restrictions << veggie
-    user.restrictions << gf
 
-    dairy.click
+    # dairy.click
 
     expect(veg).to_not be_checked
     expect(vegan).to_not be_checked
