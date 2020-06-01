a20f59b54ce92d7042a28e12c84db3d40814219b
Alex Latham <alexlthm@gmail.com>
2020-05-31 23:10:50 -0600

    Add friends method (unnecessary?)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :user_restrictions, dependent: :destroy
   has_many :restrictions, through: :user_restrictions
   has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   validates :name, presence: true
   validates :email, presence: true
