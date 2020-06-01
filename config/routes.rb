4a3c07c596002a9a23798da264bfe3c6b0ac0cf5
Alex Latham <alexlthm@gmail.com>
2020-05-31 22:50:11 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'welcome#index'
 
+  get 'auth/google_oauth2', as: 'google_login'
+  get 'auth/google_oauth2/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   resources :search, only: [:create]
-
   resources :recipes, only: [:index, :show]
-
-  resources :favorites, only: [:index, :create, :destroy]
 
   namespace :profile do
     get '/', to: 'users#show'
     get '/edit', to: 'users#edit'
-    patch '/edit/:id', to: 'users#update'
-    get '/friends', to: 'friends#index'
-    get '/friends/:id', to: 'friends#show'
-    post '/friends', to: 'friends#create'
+    patch '/', to: 'users#update'
+    resources :favorites, only: [:index, :create, :destroy]
+    resources :friends, only: [:index, :create, :destroy]
   end
 
-  get 'auth/google_oauth2', as: 'google_login'
-  get 'auth/google_oauth2/callback', to: 'sessions#create'
-
+  resources :users, only: [:show] do
+    resources :favorites, only: [:index]
+    resources :friends, only: [:index]
+  end
 end
