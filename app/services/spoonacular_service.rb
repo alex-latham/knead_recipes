5f0dbef9557ea8b9e1d4453bd6c1617eef063867
David Tran <davidttemail@gmail.com>
2020-06-02 11:12:58 -0600

    Set up microservice, rebuild cassettes

--- app/services/spoonacular_service.rb
+++ app/services/spoonacular_service.rb
@@ -1,40 +1,21 @@
 class SpoonacularService
   def complex_search(search_params)
-    params = complex_search_params(search_params)
-    get_json('/recipes/complexSearch', params)
+    get_json('/recipes/complexSearch', search_params)
   end
 
   def search_by_id(recipe_id)
-    params = { apiKey: ENV['SPOONACULAR_KEY'], includeNutrition: true }
-    get_json("/recipes/#{recipe_id}/information", params)
+    get_json("/recipe/#{recipe_id}")
   end
 
   def search_by_ids(recipe_ids)
-    params = {
-      apiKey: ENV['SPOONACULAR_KEY'],
-      ids: recipe_ids,
-      includeNutrition: true
-    }
-    get_json('/recipes/informationBulk', params)
+    params = { ids: recipe_ids }
+    get_json('/recipes', params)
   end
 
   private
 
-  def complex_search_params(search_params)
-    {
-      apiKey: ENV['SPOONACULAR_KEY'],
-      includeIngredients: search_params[:ingredients],
-      maxReadyTime: search_params[:time],
-      diet: search_params[:diet], type: search_params[:type],
-      addRecipeInformation: true, fillIngredients: true, ignorePantry: true,
-      minCalories: 0, minCholesterol: 0, minFat: 0, minProtein: 0, minCarbs: 0,
-      minSugar: 0, minSodium: 0,
-      number: 12, sort: 'min-missing-ingredients'
-    }.compact
-  end
-
   def conn
-    Faraday.new(url: 'https://api.spoonacular.com/')
+    Faraday.new(url: ENV['MICROSERVICE_URL'])
   end
 
   def get_json(url, params = nil)
