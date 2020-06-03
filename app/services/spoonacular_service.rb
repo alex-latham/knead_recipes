284991e0df6d41e651b5ad428bdb9516e16d7003
David Tran <davidttemail@gmail.com>
2020-06-03 16:33:52 -0600

    dt-refactor-wip

--- app/services/spoonacular_service.rb
+++ app/services/spoonacular_service.rb
@@ -20,6 +20,9 @@ class SpoonacularService
 
   def get_json(url, params = nil)
     response = conn.get(url, params)
+    require "pry"; binding.pry
+    return nil if response.body.empty?
+
     JSON.parse(response.body, symbolize_names: true)
   end
 end
