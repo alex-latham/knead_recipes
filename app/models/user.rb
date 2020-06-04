a0e59829b2f5defe2dba9576be6e83c3ab4efc15
David Tran <davidttemail@gmail.com>
2020-06-03 21:20:29 -0600

    Refactor with model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,10 @@ class User < ApplicationRecord
     restrictions.map(&:name)
   end
 
+  def has_favorited?(recipe_id)
+    favorites.pluck(:recipe_id).include?(recipe_id)
+  end
+
   def self.from_omniauth(response)
     find_or_create_by(email: response['info']['email']) do |user|
       user.name = response['info']['name']
