49f7de33df11287e2b3556532e993c7c7a1644cf
Alex Latham <alexlthm@gmail.com>
2020-06-04 15:36:29 -0600

    Remove commented lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => "knead-recipes.herokuapp.com" }
 
-
   # Set ActionMailer to use SendGrid
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
