592f36a1fa355bde086adfddd94e7409566b4320
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-23 11:03:28 -0700

    Begin tests for guest recipe searching

--- spec/features/guests/guest_search_spec.rb
+++ spec/features/guests/guest_search_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 describe "searching recipes" do
   it "can search recipes by an ingredient" do
-    visit guest_home_path
-    fill_in :ingredients, with: "apples, cinnamon"
-    find(:xpath, "//input[@value='vegetarian']").click
-    find(:xpath, "//input[@value='15']").click
-    find(:xpath, "//input[@value='dessert']").click
+    VCR.use_cassette('apples_cinnamon_vegetarian_time_15_dessert_3_results') do
+      visit guest_home_path
+      fill_in :ingredients, with: "apples, cinnamon"
+      find(:xpath, "//input[@value='vegetarian']").click
+      find(:xpath, "//input[@value='15']").click
+      find(:xpath, "//input[@value='dessert']").click
 
-    click_on "Search Recipes"
-    expect(page).to have_current_path("guest/recipes?diet=vegetarian&ingredients=apples%2C+cinnamon&time=15&type=dessert")
-    expect(page).to have_css('.recipes', count: 3)
+      click_on "Search Recipes"
+      expect(page).to have_current_path("/guest/recipes?diet=vegetarian&ingredients=apples%2C+cinnamon&time=15&type=dessert")
+      expect(page).to have_css('.recipes', count: 3)
+    end
   end
 
   # it "can search for recipes without any fields" do
