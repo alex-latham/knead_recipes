a4f2ea06cd2bddab7d2bcd87bb340a2d4158ca0d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-23 10:25:59 -0700

    Update flash message and create view for guest use

--- app/controllers/guest/sessions_controller.rb
+++ app/controllers/guest/sessions_controller.rb
@@ -2,7 +2,7 @@ class Guest::SessionsController < ApplicationController
   def create
     guest = Guest.new('Guest', 'guest')
     session[:guest] = true
-    flash['alert alert-success'] = "Logged in as #{guest.name}"
+    flash['alert alert-success'] = "Logged in as #{guest.name}.  To use all features of Knead Recipes you will need to log in."
     redirect_to guest_home_path
   end
 end
